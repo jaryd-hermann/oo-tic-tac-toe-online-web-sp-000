0aa4544c1d078eaef2fab25c869fa8c39571e2ec
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-03 11:52:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -112,5 +112,17 @@ end
     end
   end
 
+  def winner
+    WIN_COMBINATIONS.detect do |combo|
+    if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+      return "X"
+    elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+      return "O"
+    else
+      nil
+    end
+  end
+end
+
 
 end #end of class object
