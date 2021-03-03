edac0bed136ec5f29b8aa40af6682306a08a6e4f
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-03 11:47:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,8 +87,13 @@ class TicTacToe
     if board_empty
       false
     else
-
-
+    WIN_COMBINATIONS.each do |combo| 
+    if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" || @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+    return combo
+  end
+end
+false 
+end 
   end
 
 end #end of class object
